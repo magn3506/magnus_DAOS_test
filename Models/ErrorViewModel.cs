1bd1bb387ea2ee8161ab93128a34f30ec9dfefd9
MagnusVJensen <magnusvjensen@gmail.com>
2020-11-22 12:13:39 +0100

    intital commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace magnus_daos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
